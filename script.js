5f8915280b641d72939c9879447c3d4489d80d67
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-10-03 11:57:00 +0600

    Compelet This Project

--- script.js
+++ script.js
@@ -27,3 +27,32 @@ faqs.forEach(fag =>{
         }
     })
 })
+
+
+// open nav bar 
+
+const menu = document.querySelector('.nav_menu');
+const menuBtn1 = document.querySelector('#open-menu-btn');
+const menuBtn2 = document.querySelector('#close-menu-btn');
+
+menuBtn1.addEventListener('click',()=>{
+    menu.style.display = "flex";
+    menuBtn2.style.display = "inline-block";
+    menuBtn1.style.display = "none";
+})
+
+// Colose Nav menu
+
+const closeNav = ()=>{
+    menu.style.display = "none";
+    menuBtn2.style.display = "none"
+    menuBtn1.style.display = "inline-block";
+}
+menuBtn2.addEventListener('click',closeNav)
+
+
+// nav color change scroll
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>100)
+})
